b6ec9a9c26077fa143722285f69f2632a76ff7b1
kmcdonald <kmcdusa@gmail.com>
2025-01-26 16:58:00 -0500

    backend render file + cors

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from endpoints.league import router as league_router
 from endpoints.players import router as players_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000", 
+    "https://fpl-dashboard.com",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(players_router)
 app.include_router(league_router)
